6f4956840c11320fc9eea62a6450b1564845d761
Rui Wang <rui.wang.624@gmail.com>
2021-08-18 19:48:30 +0000

    top-bar, home and help component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './modules/shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
+import { HelpComponent } from './components/help/help.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HelpComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
