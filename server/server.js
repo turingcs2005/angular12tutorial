c32fe058f2bea2f37b1709cb3474e7e8c06ec9e1
Rui Wang <rui.wang.624@gmail.com>
2021-08-29 01:33:42 +0000

    changed port to 3006

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3006;
 
 app.use(express.static('./dist'));
 
-app.list(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Angular tutorial running on port ${PORT}.`);
 });
 
