8302f0a12c43fac2c7809e2fcf3908b1af58966c
Rui Wang <rui.wang.624@gmail.com>
2021-09-02 12:36:55 +0000

    minor change

--- src/app/services/form-factory.service.ts
+++ src/app/services/form-factory.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
