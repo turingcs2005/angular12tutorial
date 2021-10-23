d372f1db26615a47cd8200124d7c9a0462ba18f4
Rui Wang <rui.wang.624@gmail.com>
2021-10-23 10:24:22 +0000

    minor formular change

--- src/app/services/form-tools.service.ts
+++ src/app/services/form-tools.service.ts
@@ -10,7 +10,7 @@ export class FormToolsService {
   traverseTree(o: any) {
     if (o) {
       for (let [key, value] of Object.entries(o)) {
-        if (value && typeof value === 'string' && /^\$[1-9][0-9]{0,2}(\,[0-9]{3})*$/.test(value)) {
+        if (value && typeof value === 'string' && /^\$([1-9][0-9]{0,2}(\,[0-9]{3})*)|0$/.test(value)) {
           o[key] = parseInt(value.replace(/\D/g, ''), 10);
         } else {
           // 🔥 recursion 
